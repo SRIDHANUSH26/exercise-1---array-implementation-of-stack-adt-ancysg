c53e34f070dded6fe396850e6c38272034b99128
ancysg <109953797+ancysg@users.noreply.github.com>
2022-07-25 15:32:24 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,23 +3,31 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
+        
 
     def is_empty(self):
         # Write code here
+        return len(self.items)==0
 
     def is_full(self):
         # Write code here
+        return len9self.items)>self.size
 
     def push(self, data):
         if not self.is_full():
             # Write code here
+            self.items.append(data)
 
     def pop(self):
         if not self.is_empty():
             # Write code here
+            self.removed_element=self.items.pop()
+            
 
     def status(self):
         # Write code here
+        for numbers in self.items:
+            print(numbers)
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
